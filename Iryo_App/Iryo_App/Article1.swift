360186e2c9f3d3d04644c5c56c9b5162bf6c2e19
Shunya Koyama <b1013075@fun.ac.jp>
2015-06-24 15:26:37 +0900

    speakBtnを継承できるようにした

--- Iryo_App/Iryo_App/Article1.swift
+++ Iryo_App/Iryo_App/Article1.swift
@@ -7,73 +7,30 @@
 //
 
 import UIKit
-import QuartzCore
-import AVFoundation
 
-class Article1:UIViewController, AVSpeechSynthesizerDelegate{
+class Article1:BaseArticleViewController{
     
     @IBOutlet weak var scrollview: UIScrollView!
     @IBOutlet weak var speakbtn: UIButton!
-    @IBOutlet weak var voicevolume: UISlider!
-    @IBOutlet weak var voicepitch: UISlider!
+    @IBOutlet weak var voiceRateSlider: UISlider!
+    @IBOutlet weak var voicePitchSlider: UISlider!
     @IBOutlet weak var mytext: UITextView!
-    
-    let speechSynthesizer = AVSpeechSynthesizer()
-    var toggle : Bool = true
     
     override func viewDidLayoutSubviews() {
         //ScrollViewのContentSizeを設定
         self.scrollview.contentSize = self.mytext.frame.size
+        self.speaktext = mytext.text
+        self.voicerate = voiceRateSlider.value
+        self.voicepitch = voicePitchSlider.value
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        self.speechSynthesizer.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func mybtn(sender: AnyObject) {
-        
-        if toggle{
-            let utterance = AVSpeechUtterance(string: self.mytext.text)
-            
-            utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
-            utterance.rate = voicevolume.value
-            utterance.pitchMultiplier = voicepitch.value
-            
-            speechSynthesizer.speakUtterance(utterance)
-            animateActionButtonAppearance(true)
-        }else{
-            speechSynthesizer.stopSpeakingAtBoundary(AVSpeechBoundary.Immediate)
-            animateActionButtonAppearance(false)
-        }
-    }
-    
-    @IBAction func stopbutton(sender: AnyObject) {
-        speechSynthesizer.stopSpeakingAtBoundary(AVSpeechBoundary.Immediate)
-    }
-    
-    func animateActionButtonAppearance(shouldHideSpeakButton: Bool) {
-        
-        if shouldHideSpeakButton {
-            UIView.animateWithDuration(0.25, animations: { () -> Void in
-                self.speakbtn.layer.backgroundColor = UIColor(red: 255, green: 0, blue: 0, alpha: 1.0).CGColor
-                self.speakbtn.setTitle("Pause", forState: UIControlState.Normal)
-            })
-            toggle = false
-        }else{
-            UIView.animateWithDuration(0.25, animations: { () -> Void in
-                self.speakbtn.layer.backgroundColor = UIColor(red: 0.12, green: 0.78, blue: 0, alpha: 1.0).CGColor
-                self.speakbtn.setTitle("Speak", forState: UIControlState.Normal)
-            })
-            toggle = true
-        }
-        
     }
 }
---
+++ Iryo_App/Iryo_App/BaseArticleViewController.swift
@@ -0,0 +1,83 @@
+//
+//  BaseArticleViewController.swift
+//  Iryo_App
+//
+//  Created by 伊藤恵研究室 on 2015/06/24.
+//  Copyright (c) 2015年 伊藤恵研究室メンバ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class BaseArticleViewController: UIViewController, AVSpeechSynthesizerDelegate{
+    
+    var speakDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let speakBtn:UIButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+    
+    /* 音声設定 */
+    var voicerate :Float = 0.5 //速さ 0.1~1.0
+    var voicepitch :Float = 0.5 //高さ 0.5~2.0
+    var speaktext :String = "テキスト"; //読み上げるテキスト
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        speakDelegate.speechSynthesizer.delegate = self
+        
+        speakBtn.addTarget(self, action: "onClickSpeakBtn", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        /* speakBtnレイアウト設定 */
+        speakBtn.frame = CGRectMake(0, 0, 60, 30)
+        speakBtn.layer.cornerRadius = 5
+        speakBtn.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        
+        if speakDelegate.toggle{
+            speakBtn.setTitle("Speak", forState: .Normal )
+            speakBtn.layer.backgroundColor = UIColor(red: 0.12, green: 0.78, blue: 0, alpha: 1.0).CGColor
+        }else{
+            speakBtn.layer.backgroundColor = UIColor(red: 255, green: 0, blue: 0, alpha: 1.0).CGColor
+            speakBtn.setTitle("Pause", forState: UIControlState.Normal)
+        }
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: speakBtn)
+    }
+
+    
+    /** speakBtnのアニメーションを設定 */
+    func animateActionButtonAppearance(shouldHideSpeakButton: Bool) {
+        
+        if shouldHideSpeakButton {
+            UIView.animateWithDuration(0.25, animations: { () -> Void in
+                self.speakBtn.layer.backgroundColor = UIColor(red: 255, green: 0, blue: 0, alpha: 1.0).CGColor
+                self.speakBtn.setTitle("Pause", forState: UIControlState.Normal)
+            })
+            speakDelegate.toggle = false
+        }else{
+            UIView.animateWithDuration(0.25, animations: { () -> Void in
+                self.speakBtn.layer.backgroundColor = UIColor(red: 0.12, green: 0.78, blue: 0, alpha: 1.0).CGColor
+                self.speakBtn.setTitle("Speak", forState: UIControlState.Normal)
+            })
+            speakDelegate.toggle = true
+        }
+    }
+    
+    /** speakBtnを押した時 */
+    func onClickSpeakBtn() {
+        if speakDelegate.toggle{
+            let utterance = AVSpeechUtterance(string: self.speaktext)
+            utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+            utterance.rate = voicerate
+            utterance.pitchMultiplier = voicepitch
+
+            speakDelegate.speechSynthesizer.speakUtterance(utterance)
+            animateActionButtonAppearance(true)
+        }else{
+            speakDelegate.speechSynthesizer.stopSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+            animateActionButtonAppearance(false)
+        }
+    }
+    
+
+    
+}
