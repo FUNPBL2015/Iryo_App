39b94aa5a6232d44342a854626e277c358d3f839
b1013120 <b1013120@fun.ac.jp>
2015-07-02 17:41:55 +0900

    記事文字サイズ修正、記事3の文章改訂

--- Iryo_App/Iryo_App/Article3.swift
+++ Iryo_App/Iryo_App/Article3.swift
@@ -13,10 +13,7 @@ class Article3: BaseArticleViewController {
     @IBOutlet weak var myScroll: UIScrollView!
     @IBOutlet weak var myLabel1: UILabel!
     @IBOutlet weak var myLabel2: UILabel!
-    @IBOutlet weak var myText: UITextView!
-    @IBOutlet weak var comBtn: UIButton!
     @IBOutlet weak var myLabel3: UILabel!
-    @IBOutlet weak var myText2: UITextView!
     @IBOutlet weak var cheBtn: UIButton!
     
     override func viewDidLayoutSubviews() {
@@ -27,32 +24,20 @@ class Article3: BaseArticleViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.title = "病院で家族が聞かれること"
+        
         self.speaktext = "テキスト" //読み上げるテキスト
         
-        myLabel1.text="🔵 病院で医療をうけるとき\n\n  ・ご本人のふだんの様子\n\n  ・ご本人の医療行為に関する希望や意思\n\n  に関する情報がたいへん役に立ちます!"
-        myLabel1.numberOfLines=20
-        myLabel1.font=UIFont(name:"HiraKakuProN-W6",size: 28)
+        myLabel1.font=UIFont(name:"HiraKakuProN-W3",size: 28)
+        myLabel1.text="例えばこのようなとき、家族だけで判断できますか？"
         
-        myLabel2.text="ご家族で一度話し合ってみましょう！\n\n話し合いのポイント"
+        myLabel2.text="・急変したら，心臓マッサージ／血圧をあげる薬の投与／人工呼吸などの救命措置をしますか？\n・口からものを食べられないときに，胃ろう／経鼻経管栄養／中心静脈栄養／点滴をしますか？\n・腎臓の働きが悪くなったときに，人工透析をしますか？"
         myLabel2.numberOfLines=30
-        myLabel2.font=UIFont(name: "HiraKakuProN-W6", size: 30)
+        myLabel2.font=UIFont(name: "HiraKakuProN-W3", size: 28)
         
-        myText.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        myText.text="・身近な他人のケースやテレビのみとりの話題などをきっかけにすると話しやすくなります。\n\n・ご本人の希望はできるだけご本人を取り巻く皆と共有しておきましょう。\n  話し合った内容を事前指示書など書面に記録しておくのも1つです。"
-        myText.font = UIFont.systemFontOfSize(CGFloat(28))
-        
-        comBtn.setTitle("家族での話し合い例を見る", forState: UIControlState.Normal)
-        comBtn.titleLabel!.font = UIFont(name: "HiraKakuProN-W6",size: 28)
-        comBtn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        comBtn.backgroundColor = UIColor(red: 0, green: 0.5, blue: 0.8, alpha: 0.5)
-        
-        myLabel3.text="チェックリストを活用しよう！"
+        myLabel3.text="家族だけで重大な医療行為を決めるのはとても難しいです。\nふだんからよく話し合い、本人の意思や希望を確認しておくとよいでしょう。"
         myLabel3.numberOfLines=30
-        myLabel3.font=UIFont(name: "HiraKakuProN-W6", size: 30)
-        
-        myText2.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        myText2.text="このチェックリストは・・・\n\n・ご本人の認知症の状態を知るとき\n\n・病院への受診時に状態を伝えるとき\n\nに役立ちます。"
-        myText2.font = UIFont.systemFontOfSize(CGFloat(28))
+        myLabel3.font=UIFont(name: "HiraKakuProN-W3", size: 28)
         
         cheBtn.setTitle("チェックリストをやる", forState: UIControlState.Normal)
         cheBtn.titleLabel!.font = UIFont(name: "HiraKakuProN-W6",size: 28)
