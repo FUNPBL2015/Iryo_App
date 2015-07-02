704bcb2b17ddba45c774f76e93f8940583d610ba
b1013120 <b1013120@fun.ac.jp>
2015-07-02 17:05:21 +0900

    記事1を修正

--- Iryo_App/Iryo_App/Article1.swift
+++ Iryo_App/Iryo_App/Article1.swift
@@ -10,20 +10,33 @@ import UIKit
 
 class Article1:BaseArticleViewController{
     
-    @IBOutlet weak var scrollview: UIScrollView!
+    //@IBOutlet weak var scrollview: UIScrollView!
     //@IBOutlet weak var speakbtn: UIButton!
     //@IBOutlet weak var voiceRateSlider: UISlider!
     //@IBOutlet weak var voicePitchSlider: UISlider!
-    @IBOutlet weak var mytext: UITextView!
+    //@IBOutlet weak var mytext: UITextView!
     
+    /*
     override func viewDidLayoutSubviews() {
         //ScrollViewのContentSizeを設定
         self.scrollview.contentSize = self.mytext.frame.size //contentSizeをtextViewに合わせる
-    }
+    }*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //textview生成
+        let mytext: UITextView = UITextView(frame: CGRectMake(16, 16, self.view.frame.width - 32, 500))
+        mytext.text = "もし認知症のおじいちゃん、おばあちゃんが急に入院して検査や手術を受けることになったら… \n・本人の　同意　が得られない\n・本人の　意思　がわからない\n・家族の　同意　だけで医療行為が進められる\n・場合によっては、本人の同意が得られないために、必要な医療行為が受けられない\nということもありえます\nふだんから本人の意思や希望、気持ちを確認しておくことが大切です！\n\nこのアプリは、本人と家族が納得のいく医療を受けられるように…\n・ふだんの生活や症状の共有方法\n・病院で家族が聞かれること\n・医師と家族の話し合い\n・家族の今後の生活\nについて、アドバイスします！"
+        mytext.font = UIFont.systemFontOfSize(CGFloat(28))  //フォント・サイズ設定
+        mytext.font = UIFont(name:"HiraKakuProN-W3", size:UIFont.labelFontSize())
+        mytext.textColor = UIColor.blackColor()             //文字色
+        mytext.textAlignment = NSTextAlignment.Left         //左詰め
+        mytext.editable = false         //編集禁止
+        self.view.addSubview(mytext)    //viewに追加
+        
         
         //mytext.text = "test"
         mytext.layer.borderWidth = 0
