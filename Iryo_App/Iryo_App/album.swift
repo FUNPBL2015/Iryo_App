e835f4518a1995377627ed2652a2f21a94237598
Shunya Koyama <b1013075@fun.ac.jp>
2015-11-27 15:44:27 +0900

    Parseライブラリのimport

--- Iryo_App/Iryo_App/album.swift
+++ Iryo_App/Iryo_App/album.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class album: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
--- Iryo_App/Iryo_App/picture2.swift
+++ Iryo_App/Iryo_App/picture2.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class picture2: UIViewController{
     
